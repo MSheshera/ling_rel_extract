8c1d784a0075a1e61a1c20c4a8680164ba5126e7
MSheshera <mssheshera@yahoo.com>
2017-11-21 17:40:41 -0500

    (m) Make changes to write to read processed data. Will change.

--- pre_process/run_explore_data.sh
+++ pre_process/run_explore_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # $CUR_PROJ_ROOT is a environment variable; manually set outside of the script.
-data_path="$CUR_PROJ_ROOT/datasets/google-relation-extraction/google-relation-extraction"
+data_path="$CUR_PROJ_ROOT/processed"
 log_path="$CUR_PROJ_ROOT/logs/pre_process"
 mkdir -p $log_path
 
@@ -8,4 +8,4 @@ source_path="$CUR_PROJ_ROOT/source/"
 
 cmd="python2 -u $source_path/pre_process/explore_data.py $data_path"
 echo $cmd | tee "$log_path/explore_data_logs.txt"
-eval $cmd | tee -a "$log_path/explore_data_logs.txt"
+eval $cmd > "$log_path/explore_data_logs.txt"
