cab8a1364d7986f0bf7d511587d168ebf18cad85
MSheshera <mssheshera@yahoo.com>
2017-12-10 23:32:02 -0500

    Add changes calling with cl_args; better hyperparameter unpacking.

--- models/main.py
+++ models/main.py
@@ -5,7 +5,7 @@ sure training is doing something meaningful.
 from __future__ import unicode_literals
 from __future__ import print_function
 import argparse, os, sys
-import math, time, codecs
+import math, time, codecs, pprint
 
 import utils
 import model_utils as mu
@@ -17,8 +17,8 @@ import evaluate
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
 
-def train_model(int_mapped_path, embedding_path, model_path,
-                result_path, use_toy=True):
+def train_model(int_mapped_path, embedding_path, run_path,
+                model_hparams, train_hparams, use_toy=True):
     """
     Read the int training and dev data, initialize and train the model.
     :return:
@@ -35,26 +35,39 @@ def train_model(int_mapped_path, embedding_path, model_path,
             'y_train': y_train,
             'X_dev': X_dev,
             'y_dev': y_dev}
+    # Unpack args.
+    edim, hdim, dropp = model_hparams['edim'], model_hparams['hdim'], model_hparams['dropp']
+    bsize, epochs, lr = train_hparams['bsize'], train_hparams['epochs'], train_hparams['lr']
+    print('Model hyperparams:')
+    pprint.pprint(model_hparams)
+    print('Train hyperparams:')
+    pprint.pprint(train_hparams)
 
     # Initialize model.
     model = slstm.SentsLSTM(word2idx, embedding_path, num_classes=6,
-                            num_layers=1, embedding_dim=200, hidden_dim=50,
-                            dropout=0.3)
+                            num_layers=1, embedding_dim=edim, hidden_dim=hdim,
+                            dropout=dropp)
     # Initialize the trainer.
     lstmtrainer = trainer.LSTMTrainer(
-        model=model, data=data, batcher=mu.Batcher, batch_size=16,
-        update_rule='adam', num_epochs=5, learning_rate=0.001, check_every=10,
-        print_every=1, model_path=model_path)
+        model=model, data=data, batcher=mu.Batcher, batch_size=bsize,
+        update_rule='adam', num_epochs=epochs, learning_rate=lr, check_every=10,
+        print_every=10, model_path=run_path)
 
     # Train and save the best model to model_path.
     lstmtrainer.train()
-    # Make predictions and evaluate.
+    # Plot training time stats.
+    utils.plot_train_hist(lstmtrainer.loss_history, lstmtrainer.checked_iters,
+                          fig_path=run_path, ylabel='Batch loss')
+    utils.plot_train_hist(lstmtrainer.dev_score_history,
+                          lstmtrainer.checked_iters,
+                          fig_path=run_path, ylabel='Dev F1')
+
+    # Make predictions and evaluate on all the data.
     data['X_test'] = X_test
     data['y_test'] = y_test
     everything = evaluate.make_predictions(data=data, batcher=mu.Batcher,
-                                           model=model,
-                                           model_name=lstmtrainer.model_name,
-                                           result_path=result_path)
+                                           model=model, result_path=run_path,
+                                           batch_size=128, write_preds=True)
     y_test_true, y_test_preds, y_dev_true, y_dev_preds, \
         y_train_true, y_train_preds = everything
     for split, true, pred in [('test', y_test_true, y_test_preds),
@@ -65,9 +78,49 @@ def train_model(int_mapped_path, embedding_path, model_path,
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest=u'subcommand',
+                                       help=u'The action to perform.')
+    # Train the model.
+    train_args = subparsers.add_parser(u'train_model')
+    # Where to get what.
+    train_args.add_argument(u'--int_mapped_path', required=True,
+                            help=u'Path to the int mapped dataset.')
+    train_args.add_argument(u'--embedding_path', required=True,
+                            help=u'Path to the glove embeddings directory.')
+    train_args.add_argument(u'--run_path', required=True,
+                            help=u'Path to directory to save all run items to.')
+    # Model hyper-parameters.
+    train_args.add_argument(u'--edim', required=True, type=int,
+                            choices=[200], help=u'Embedding dimension.')
+    train_args.add_argument(u'--hdim', required=True, type=int,
+                            choices=range(1, 201, 1),
+                            help=u'LSTM hidden dimension.')
+    train_args.add_argument(u'--dropp', required=True, type=float,
+                            choices=[0.3], help=u'Dropout probability.')
+    # Training hyper-parameters.
+    train_args.add_argument(u'--bsize', required=True, type=int,
+                            choices=[8, 16, 32, 64, 128, 256],
+                            help=u'Batch size.')
+    train_args.add_argument(u'--epochs', required=True, type=int,
+                            choices=range(1, 11, 1),
+                            help=u'Number of training epochs.')
+    train_args.add_argument(u'--lr', required=True, type=float,
+                            choices=[0.1, 0.01, 0.001, 0.0001, 0.00001],
+                            help=u'Learning rate.')
+    cl_args = parser.parse_args()
+
+    if cl_args.subcommand == 'train_model':
+        model_hparams, train_hparams = {}, {}
+        model_hparams['edim'], model_hparams['hdim'], model_hparams['dropp'] = \
+            cl_args.edim, cl_args.hdim, cl_args.dropp
+        train_hparams['bsize'], train_hparams['epochs'], train_hparams['lr'] = \
+            cl_args.bsize, cl_args.epochs, cl_args.lr
+        train_model(int_mapped_path=cl_args.int_mapped_path,
+                    embedding_path=cl_args.embedding_path,
+                    run_path=cl_args.run_path,
+                    model_hparams=model_hparams, train_hparams=train_hparams,
+                    use_toy=False)
 
 if __name__ == '__main__':
-    #main()
-    train_model(int_mapped_path=sys.argv[1], embedding_path=sys.argv[2],
-                model_path=sys.argv[3], result_path=sys.argv[4])
+    main()
--- models/run_main.sh
+++ models/run_main.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
-cmd="python2 -u main.py /mnt/nfs/work1/mccallum/smysore/ling_rel_extract/processed/grec-slstm \
-    /mnt/nfs/work1/mccallum/smysore/ling_rel_extract/datasets/embeddings/glove \
-    /mnt/nfs/work1/mccallum/smysore/ling_rel_extract/saved_models \
-    /mnt/nfs/work1/mccallum/smysore/ling_rel_extract/results"
 
-eval $cmd | tee "/mnt/nfs/work1/mccallum/smysore/ling_rel_extract/logs/models/test.txt"
+# Model name which is directory where everything from a specific run is saved.
+model_name=$1
+if [[ $model_name == '' ]]; then
+	echo "Specify a meaningful model name; dir where all run data is saved."
+	exit 1
+fi
+run_time=`date '+%Y_%m_%d-%H_%M_%S'`
+int_mapped_path="$CUR_PROJ_ROOT/processed/grec-slstm"
+glove_path="$CUR_PROJ_ROOT/datasets/embeddings/glove"
+run_path="$CUR_PROJ_ROOT/model_runs/${run_time}-${model_name}"
+mkdir -p $run_path
+
+cmd="python2 -u main.py  train_model --int_mapped_path $int_mapped_path \
+     --embedding_path $glove_path \
+     --run_path $run_path \
+     --edim 200 --hdim 50 --dropp 0.3 \
+     --bsize 32 --epochs 5 --lr 0.001"
+
+echo $cmd | tee "$run_path/run_log.txt"
+eval $cmd | tee -a "$run_path/run_log.txt"
+
+# Move figures to easy to rsync directory. >_<
+cp $run_path/*.png $CUR_PROJ_ROOT/rsync_results/
+cp $run_path/*.eps $CUR_PROJ_ROOT/rsync_results/
