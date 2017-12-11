e9c4ad9d08f69649f8010c43f397452c8e8ff0bf
MSheshera <mssheshera@yahoo.com>
2017-12-11 08:15:33 -0500

    Add changes to read trained doc2vec + tune reg param for logistic.

--- models/baseline.py
+++ models/baseline.py
@@ -1,11 +1,15 @@
 """
 Add code for the baseline model; doc2vec with gensim + logistic regression.
+Tutorials etc:
+https://medium.com/@mishra.thedeepak/doc2vec-in-a-simple-way-fa80bfe81104
+http://linanqiu.github.io/2015/10/07/word2vec-sentiment/
+Doc2vec hparams from: https://github.com/jhlau/doc2vec
 """
 from __future__ import print_function
 from __future__ import unicode_literals
 import os, sys, argparse
 import codecs
-import time, random
+import time, random, copy
 
 import numpy as np
 from sklearn import linear_model
@@ -80,6 +84,7 @@ def run_baseline(vlstm_grec_path, model_path):
                        iter=train_epoch)
     print(model)
     print('Doc2Vec train time: {:.4f}'.format(time.time()-start))
+    model.save(os.path.join(model_path, 'grec_lstm.d2v'))
     print()
     doc_vecs = model.docvecs[docs_id]
 
@@ -139,5 +144,85 @@ def run_baseline(vlstm_grec_path, model_path):
                                 'Train confusion matrix')
 
 
+def load_baseline(vlstm_grec_path, model_path):
+    """
+    Load trained doc2vec and make predictions.
+    :param vlstm_grec_path:
+    :param model_path:
+    :return:
+    """
+    # read the data.
+    docs_id, docs, docs_label, train_id, dev_id, test_id = \
+        read_grec(vlstm_grec_path)
+    # Read model.
+    model = gm.Doc2Vec.load(os.path.join(model_path, 'grec_lstm.d2v'))
+    doc_vecs = model.docvecs[docs_id]
+    print('Doc vecs: {}'.format(doc_vecs.shape))
+
+    # Form X, Y pairs.
+    train_X, train_y = doc_vecs[train_id:dev_id, :], np.array(
+        docs_label[train_id:dev_id])
+    dev_X, dev_y = doc_vecs[dev_id:test_id, :], np.array(
+        docs_label[dev_id:test_id])
+    test_X, test_y = doc_vecs[test_id:, :], np.array(docs_label[test_id:])
+    print(train_X.shape, dev_X.shape, test_X.shape)
+
+    # Training logistic regression.
+    print('Training LR')
+    start = time.time()
+    best_clf = None
+    best_score = 0
+    for cur_C in [100, 50, 10, 5, 1, 0.1, 0.01]:
+        clf = linear_model.LogisticRegression(
+            penalty='l2', dual=False, tol=1e-4, C=cur_C, fit_intercept=True,
+            intercept_scaling=1, solver='lbfgs', max_iter=100,
+            multi_class='multinomial', verbose=10, warm_start=False, n_jobs=1)
+        print(clf)
+        clf.fit(train_X, train_y)
+        print('LR train time: {:.4f}'.format(time.time() - start))
+        dev_preds = clf.predict(dev_X)
+        f1_score = metrics.f1_score(dev_y, dev_preds, average='weighted')
+        print('C: {:.4f}; f1: {:.4f}'.format(cur_C, f1_score))
+        if f1_score > best_score:
+            best_score = f1_score
+            best_clf = copy.deepcopy(clf)
+        print()
+    clf = best_clf
+
+    # Make predictions and save.
+    train_preds = clf.predict(train_X)
+    dev_preds = clf.predict(dev_X)
+    test_preds = clf.predict(test_X)
+
+    print('test')
+    test_preds_file = os.path.join(model_path, 'test_preds_best.npy')
+    evaluate.write_predictions(test_preds_file, test_y, test_preds)
+
+    print('dev')
+    dev_preds_file = os.path.join(model_path, 'dev_preds_best.npy')
+    evaluate.write_predictions(dev_preds_file, dev_y, dev_preds)
+
+    print('train')
+    train_preds_file = os.path.join(model_path, 'train_preds_best.npy')
+    evaluate.write_predictions(train_preds_file, train_y, train_preds)
+
+    # Evaluate.
+    evaluate.evaluate_preds(train_y, train_preds)
+    evaluate.evaluate_preds(dev_y, dev_preds)
+    evaluate.evaluate_preds(test_y, test_preds)
+
+    # Plot confusion matrices.
+    class_labels = ['institution', 'place_of_birth', 'place_of_death',
+                    'date_of_birth', 'education-degree', 'no_relation']
+    dev_cm = metrics.confusion_matrix(dev_y, dev_preds)
+    train_cm = metrics.confusion_matrix(train_y, train_preds)
+    utils.plot_confusion_matrix(dev_cm, class_labels, model_path,
+                                'Dev confusion matrix-best')
+    utils.plot_confusion_matrix(train_cm, class_labels, model_path,
+                                'Train confusion matrix-best')
+    print('best model')
+    print(clf)
+
 if __name__ == '__main__':
-    run_baseline(sys.argv[1], sys.argv[2])
+    #run_baseline(sys.argv[1], sys.argv[2])
+    load_baseline(sys.argv[1], sys.argv[2])
--- models/run_baseline.sh
+++ models/run_baseline.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cmd="python2 -u baseline.py /mnt/nfs/work1/mccallum/smysore/ling_rel_extract/processed/grec-vlstm /mnt/nfs/work1/mccallum/smysore/ling_rel_extract/model_runs/baseline_run"
-echo $cmd | tee "/mnt/nfs/work1/mccallum/smysore/ling_rel_extract/logs/models/baseline_logs.txt"
-eval $cmd | tee -a "/mnt/nfs/work1/mccallum/smysore/ling_rel_extract/logs/models/baseline_logs.txt"
+echo $cmd | tee "/mnt/nfs/work1/mccallum/smysore/ling_rel_extract/logs/models/load_baseline_best_logs.txt"
+eval $cmd | tee -a "/mnt/nfs/work1/mccallum/smysore/ling_rel_extract/logs/models/load_baseline_best_logs.txt"
