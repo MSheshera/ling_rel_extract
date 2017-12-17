8cb8f4fe01c3f8beaffdcb215def5ba76d971274
MSheshera <mssheshera@yahoo.com>
2017-12-17 00:22:42 -0500

    Add changes to log hparams better for hparam sweep; add function to read and predict with saved model.

--- models/main.py
+++ models/main.py
@@ -5,8 +5,9 @@ sure training is doing something meaningful.
 from __future__ import unicode_literals
 from __future__ import print_function
 import argparse, os, sys
-import math, time, codecs, pprint
+import math, time, codecs, pprint, json
 
+import torch
 import utils
 import model_utils as mu
 import sentslstm as slstm
@@ -17,8 +18,8 @@ import evaluate
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
 
-def train_model(int_mapped_path, embedding_path, run_path,
-                model_hparams, train_hparams, use_toy=True):
+def train_model(int_mapped_path, embedding_path, run_path, model_hparams,
+                train_hparams, use_toy=True):
     """
     Read the int training and dev data, initialize and train the model.
     :return:
@@ -70,11 +71,61 @@ def train_model(int_mapped_path, embedding_path, run_path,
                                            batch_size=128, write_preds=True)
     y_test_true, y_test_preds, y_dev_true, y_dev_preds, \
         y_train_true, y_train_preds = everything
+    run_info = {'model_hparams': model_hparams,
+                'train_hparams': train_hparams}
+    for split, true, pred in [('test', y_test_true, y_test_preds),
+                              ('dev', y_dev_true, y_dev_preds),
+                              ('train', y_train_true, y_train_preds)]:
+        print(split)
+        wf1, wp, wr, ac = evaluate.evaluate_preds(true, pred)
+        run_info['{:s}_performance'.format(split)] = \
+            {'wf1': wf1, 'wp': wp, 'wr': wr, 'ac': ac}
+
+    # Save hyperparams and results to disk.
+    with codecs.open(os.path.join(run_path, 'run_info.json'), 'w', 'utf-8') as fp:
+        json.dump(run_info, fp)
+
+
+def run_saved_model(int_mapped_path, embedding_path, run_path, use_toy=True):
+    """
+    Read the int training and dev data, initialize and run a saved model.
+    :return:
+    """
+    # Load training and dev data.
+    X_train, y_train, X_dev, y_dev, X_test, y_test, word2idx = \
+        utils.load_intmapped_data(int_mapped_path, use_toy)
+    data = {'X_train': X_train,
+            'y_train': y_train,
+            'X_dev': X_dev,
+            'y_dev': y_dev}
+    # Load the hyperparams from disk.
+    with codecs.open(os.path.join(run_path, 'run_info.json'), 'r', 'utf-8') as fp:
+        run_info = json.load(fp)
+        model_hparams = run_info['model_hparams']
+    # Unpack args.
+    edim, hdim, dropp = model_hparams['edim'], model_hparams['hdim'], model_hparams['dropp']
+    print('Model hyperparams:')
+    pprint.pprint(model_hparams)
+
+    # Initialize model.
+    model = slstm.SentsLSTM(word2idx, embedding_path, num_classes=6,
+                            num_layers=1, embedding_dim=edim, hidden_dim=hdim,
+                            dropout=dropp)
+    model_file = os.path.join(run_path, 'slstm_best.pt')
+    model.load_state_dict(torch.load(model_file))
+    data['X_test'] = X_test
+    data['y_test'] = y_test
+    everything = evaluate.make_predictions(data=data, batcher=mu.Batcher,
+                                           model=model, result_path=run_path,
+                                           batch_size=128, write_preds=True)
+    y_test_true, y_test_preds, y_dev_true, y_dev_preds, \
+        y_train_true, y_train_preds = everything
     for split, true, pred in [('test', y_test_true, y_test_preds),
                               ('dev', y_dev_true, y_dev_preds),
                               ('train', y_train_true, y_train_preds)]:
         print(split)
         evaluate.evaluate_preds(true, pred)
+    print('Run from: {:s}'.format(run_path))
 
 
 def main():
@@ -92,22 +143,33 @@ def main():
                             help=u'Path to directory to save all run items to.')
     # Model hyper-parameters.
     train_args.add_argument(u'--edim', required=True, type=int,
-                            choices=[200], help=u'Embedding dimension.')
+                            choices=[50, 100, 200, 300],
+                            help=u'Embedding dimension.')
     train_args.add_argument(u'--hdim', required=True, type=int,
                             choices=range(1, 201, 1),
                             help=u'LSTM hidden dimension.')
     train_args.add_argument(u'--dropp', required=True, type=float,
-                            choices=[0.3], help=u'Dropout probability.')
+                            choices=[0., 0.1, 0.2, 0.3, 0.4,
+                                     0.5, 0.6, 0.7, 0.8, 0.9],
+                            help=u'Dropout probability.')
     # Training hyper-parameters.
     train_args.add_argument(u'--bsize', required=True, type=int,
-                            choices=[8, 16, 32, 64, 128, 256],
-                            help=u'Batch size.')
+                            choices=[32], help=u'Batch size.')
     train_args.add_argument(u'--epochs', required=True, type=int,
-                            choices=range(1, 11, 1),
-                            help=u'Number of training epochs.')
+                            choices=[5], help=u'Number of training epochs.')
     train_args.add_argument(u'--lr', required=True, type=float,
                             choices=[0.1, 0.01, 0.001, 0.0001, 0.00001],
                             help=u'Learning rate.')
+
+    # Run a saved model.
+    savedrun_args = subparsers.add_parser(u'run_saved_model')
+    # Where to get what.
+    savedrun_args.add_argument(u'--int_mapped_path', required=True,
+                               help=u'Path to the int mapped dataset.')
+    savedrun_args.add_argument(u'--embedding_path', required=True,
+                               help=u'Path to the glove embeddings directory.')
+    savedrun_args.add_argument(u'--run_path', required=True,
+                               help=u'Path to directory with all run items.')
     cl_args = parser.parse_args()
 
     if cl_args.subcommand == 'train_model':
@@ -121,6 +183,11 @@ def main():
                     run_path=cl_args.run_path,
                     model_hparams=model_hparams, train_hparams=train_hparams,
                     use_toy=False)
+    elif cl_args.subcommand == 'run_saved_model':
+        run_saved_model(int_mapped_path=cl_args.int_mapped_path,
+                        embedding_path=cl_args.embedding_path,
+                        run_path=cl_args.run_path,
+                        use_toy=False)
 
 if __name__ == '__main__':
     main()
--- models/run_main.sh
+++ models/run_main.sh
@@ -1,26 +1,41 @@
 #!/usr/bin/env bash
 
-# Model name which is directory where everything from a specific run is saved.
-model_name=$1
-if [[ $model_name == '' ]]; then
-	echo "Specify a meaningful model name; dir where all run data is saved."
-	exit 1
+action=$1
+run_path=$2
+if [[ $action == '' ]]; then
+    echo "Must specify action: run_saved/train_model"
+    exit 1
 fi
+if [[ $run_path == '' ]] && [[ $action == 'run_saved' ]] ; then
+    echo "Must specify full path to trained model."
+    exit 1
+fi
+
+
+edim=200
+hdim=50
+dropp=0.3
+lr=0.001
 run_time=`date '+%Y_%m_%d-%H_%M_%S'`
 int_mapped_path="$CUR_PROJ_ROOT/processed/grec-slstm"
 glove_path="$CUR_PROJ_ROOT/datasets/embeddings/glove"
-run_path="$CUR_PROJ_ROOT/model_runs/${run_time}-${model_name}"
-mkdir -p $run_path
+if [[ action == 'train_model' ]]; then
+    run_path="$CUR_PROJ_ROOT/model_runs/${run_time}-${action}"
+    mkdir -p $run_path
+fi
 
-cmd="python2 -u main.py  train_model --int_mapped_path $int_mapped_path \
-     --embedding_path $glove_path \
-     --run_path $run_path \
-     --edim 200 --hdim 50 --dropp 0.3 \
-     --bsize 32 --epochs 5 --lr 0.001"
-
-echo $cmd | tee "$run_path/run_log.txt"
-eval $cmd | tee -a "$run_path/run_log.txt"
-
-# Move figures to easy to rsync directory. >_<
-cp $run_path/*.png $CUR_PROJ_ROOT/rsync_results/
-cp $run_path/*.eps $CUR_PROJ_ROOT/rsync_results/
+if [[ $action == 'train_model' ]]; then
+    cmd="python2 -u main.py  train_model --int_mapped_path $int_mapped_path \
+         --embedding_path $glove_path \
+         --run_path $run_path \
+         --edim $edim --hdim $hdim --dropp $dropp \
+         --bsize 32 --epochs 5 --lr $lr"
+    echo $cmd | tee "$run_path/train_run_log.txt"
+    eval $cmd | tee -a "$run_path/train_run_log.txt"
+elif [[ $action == 'run_saved' ]]; then
+    cmd="python2 -u main.py  run_saved_model --int_mapped_path $int_mapped_path \
+         --embedding_path $glove_path \
+         --run_path $run_path"
+    echo $cmd | tee "$run_path/saved_run_log.txt"
+    eval $cmd | tee -a "$run_path/saved_run_log.txt"
+fi
